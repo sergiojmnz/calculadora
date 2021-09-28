b1ef20b4cd9f1f6a4f796c0112172ff2eb4652b6
sergiojmnz <61261358+sergiojmnz@users.noreply.github.com>
2021-09-27 22:47:06 -0500

    asas

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,11 @@
-var base;
-var altura;
-var area;
+function validarformulario(){
+	var i;	
+	for (i = 0; i<document.miForm.checkbox.length; i++){			
+		if(document.miForm.checkbox[i].checked == true){
+			this.calcular();
+		}	
+	}	
+}
 
 function calcular(){
 	var num1Element = document.getElementById("num1");
@@ -9,20 +14,32 @@ function calcular(){
 	n2 = num2Element.value;
 	console.log(n1);
 	console.log(n2);
-
-	if (document.getElementById('sumar').checked){
-		alert('suma seleccionada');
-		parseInt("n1", 1);
-		parseInt("n2", 2);
-		resultado = n1 + n2;
+	if (isNaN(n1) || isNaN(n2) || n1.length==0 || n2.length == 0){
+		alert('Rellena todos los campos con numeros validos');
+	}else{
+		if (document.getElementById('sumar').checked){
+			resultado = parseFloat(n1) + parseFloat(n2);	
+		}
+		if (document.getElementById('restar').checked){
+			resultado = parseFloat(n1) - parseFloat(n2);	
+		}
+		if (document.getElementById('mul').checked){
+			resultado = parseFloat(n1) * parseFloat(n2);
+		}
+		if (document.getElementById('div').checked){
+			resultado = parseFloat(n1) / parseFloat(n2);
+		}
+		if (document.getElementById('cua').checked){
+			resultado = Math.pow(parseFloat(n1), 2);
+		}	
+		if (document.getElementById('sqrt').checked){
+			resultado = Math.sqrt(parseFloat(n1));
+		}
+		if (document.getElementById('pow').checked){
+			resultado = Math.pow(parseFloat(n1), parseFloat(n2));
+		}
 		console.log(resultado);
+		var resultadoElement = document.getElementById("resultado");	
+		resultadoElement.innerHTML = 'Resultado = ' + resultado;
+		}
 	}
-
-	
-
-	
-
-	var resultadoElement = document.getElementById("resultado");
-	
-	resultadoElement.innerHTML = 'Resultado = ' + resultado;
-}
